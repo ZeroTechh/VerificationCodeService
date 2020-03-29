b3f4cb072d2c623c8fe66415095d032702028882
Roshan Mehta <sonicroshan122@gmail.com>
2020-03-29 11:01:38 +0530

    Added main.go file

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"go.uber.org/zap"
+
+	"github.com/ZeroTechh/VelocityCore/logger"
+	proto "github.com/ZeroTechh/VelocityCore/proto/VerificationCodeService"
+	"github.com/ZeroTechh/VelocityCore/services"
+	"github.com/ZeroTechh/VelocityCore/utils"
+	"github.com/ZeroTechh/hades"
+
+	"github.com/ZeroTechh/VerificationCodeService/serviceHandler"
+)
+
+func main() {
+	// Loading the logger
+	config := hades.GetConfig("main.yaml", []string{"config"})
+	log := logger.GetLogger(
+		config.Map("service").Str("logFile"),
+		config.Map("service").Bool("debug"),
+	)
+
+	defer utils.HandlePanic(log)
+
+	grpcServer, listner := utils.CreateGRPCServer(
+		services.EmailVerificationSrv,
+		log,
+	)
+
+	handler := serviceHandler.Handler{}
+
+	proto.RegisterVerificationCodeServer(grpcServer, handler)
+
+	log.Info("Service Started")
+	if err := grpcServer.Serve(*listner); err != nil {
+		log.Fatal("Service Failed With Error", zap.Error(err))
+	}
+}
