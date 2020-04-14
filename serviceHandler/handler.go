4ff23c1eae985ea4a9a0bceebadff927114c794f
Roshan Mehta <sonicroshan122@gmail.com>
2020-04-14 22:15:44 +0530

    Removed logging from service handler

--- serviceHandler/handler.go
+++ serviceHandler/handler.go
@@ -3,21 +3,9 @@ package serviceHandler
 import (
 	"context"
 
-	"github.com/ZeroTechh/VelocityCore/logger"
 	proto "github.com/ZeroTechh/VelocityCore/proto/VerificationCodeService"
-	"github.com/ZeroTechh/blaze"
-	"github.com/ZeroTechh/hades"
-	"go.uber.org/zap"
 
 	"github.com/ZeroTechh/VerificationCodeService/core/code"
-)
-
-var (
-	config = hades.GetConfig("main.yaml", []string{"config", "../config"})
-	log    = logger.GetLogger(
-		config.Map("service").Str("logFile"),
-		config.Map("service").Bool("debug"),
-	)
 )
 
 // Handler is used to handle all verification code service functions
@@ -29,17 +17,7 @@ type Handler struct {
 func (handler Handler) Create(
 	ctx context.Context,
 	request *proto.UserData) (*proto.TokenData, error) {
-	funcLog := blaze.NewFuncLog(
-		"VerificationCodeService.Handler.Create",
-		log,
-		zap.String("UserID", request.UserID),
-	)
-	funcLog.Started()
-
 	token := handler.code.Create(request.UserID)
-
-	funcLog.Completed(zap.String("Token", token))
-
 	return &proto.TokenData{Token: token}, nil
 }
 
@@ -47,15 +25,6 @@ func (handler Handler) Create(
 func (handler Handler) Validate(
 	ctx context.Context,
 	request *proto.TokenData) (*proto.Valid, error) {
-	funcLog := blaze.NewFuncLog(
-		"VerificationCodeService.Handler.Validate",
-		log,
-		zap.String("Token", request.Token),
-	)
-	funcLog.Started()
-
 	valid, userID := handler.code.Validate(request.Token)
-
-	funcLog.Completed(zap.Bool("Valid", valid))
 	return &proto.Valid{Valid: valid, UserID: userID}, nil
 }
